a2b3b9c0b3794ebc7db3398723c3a490f0064428
jeremy0519 <fushantangzp201403@126.com>
2024-08-04 17:34:33 +0800

    initial setup

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Import our custom CSS
+import './scss/styles.scss'
+// Import all of Bootstrap's JS
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faUserSecret)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.mount('#app')
